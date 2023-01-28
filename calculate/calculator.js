07f8106c649b1f02991ba602905099d85ae212f8
Manasyanrs <radikmanasyan@gmail.com>
2023-01-28 09:19:53 +0400

    Have changed '/' block

--- calculate/calculator.js
+++ calculate/calculator.js
@@ -8,14 +8,14 @@ let flag = true;
 let reapitQuestion;
 
 
-while(flag){
+while (flag) {
     do {
         firsDigit = +prompt("Enter first digit: ");
-        if (!firsDigit) {
+        if (!firsDigit && firsDigit != 0) {
             confirm(`Is not true!\nNoN is not digit.`);
         }
-    } while (!firsDigit);
-    
+    } while (!firsDigit && firsDigit != 0);
+
     do {
         description = prompt("Enter description symbol: ");
         for (let index = 0; index < symbols.length; index++) {
@@ -27,50 +27,55 @@ while(flag){
         if (flag) {
             confirm(`Is not true!\nEnter the Math symbols (+, -, /, *, ^, %)`);
         }
-    
+
     } while (flag);
-    
+
     do {
         secondDigit = +prompt("Enter second digit: ");
-        if (!secondDigit) {
+        if (!secondDigit && secondDigit != 0) {
             confirm(`Is not true!\nNoN is not digit.`);
         }
-    
-    } while (!secondDigit);
-    
+
+    } while (!secondDigit && secondDigit != 0);
+
     switch (description) {
         case "+": {
             console.log(`${firsDigit} ${description} ${secondDigit} = ${firsDigit + secondDigit}`);
             break;
         }
-    
+
         case "-": {
             console.log(`${firsDigit} ${description} ${secondDigit} = ${firsDigit - secondDigit}`);
             break;
         }
-    
+
         case "/": {
+            if(secondDigit === 0){
+                console.log("Exception: divide by zero");
+                break;
+            }
+
             console.log(`${firsDigit} ${description} ${secondDigit} = ${firsDigit / secondDigit}`);
             break;
         }
-    
+
         case "*": {
             console.log(`${firsDigit} ${description} ${secondDigit} = ${firsDigit * secondDigit}`);
             break;
         }
-    
+
         case "^": {
             console.log(`${firsDigit} ${description} ${secondDigit} = ${firsDigit ** secondDigit}`);
             break;
         }
-    
+
         case "%": {
             console.log(`${firsDigit} ${description} ${secondDigit} = ${firsDigit % secondDigit}`);
             break;
         }
     }
     continueCalculate = prompt("Continue to calculate? (Y/N) ")
-    if (continueCalculate === "y" || continueCalculate === "Y"){
+    if (continueCalculate === "y" || continueCalculate === "Y") {
         flag = true;
     }
 }
