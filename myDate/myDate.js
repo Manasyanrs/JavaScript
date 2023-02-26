6b890b484a360627ab0f513d8e4a8f44a8788c60
Manasyanrs <radikmanasyan@gmail.com>
2023-02-26 04:07:24 +0400

    refresh all methods

--- myDate/myDate.js
+++ myDate/myDate.js
@@ -1,18 +1,131 @@
 "use strict"
 
-function MyDate(year, month, date, hour, minuts, second) {
-
+function MyDate(year = 1970, month = 0, days = 1, hour = 0, minutes = 0, second = 0) {
+    const strMonth = ["Yan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dev"]
     let self = this;
-    this.year = checkYear();
 
-    this.month = checkMonth();
-    this.date = checkDate();
+    this.year = checkYear();
+    this.month = month;
+    this.days = days;
 
     this.hour = hour;
-    this.minuts = minuts;
-    this.second = second;
+    this.minutes = minutes;
+    this.second = second(second);
 
 
+
+    this.setFullYear = function (yearValue, monthValue, day) {
+        if (typeof yearValue != "number" && typeof monthValue != "number" && typeof day != "number") {
+            self.year = 2023;
+            self.month = 1;
+            self.day = 1;
+
+        } else if (typeof yearValue === "number" && typeof monthValue === "number" && typeof day === "number") {
+            self.year = yearValue;
+            days = checkDays(day);
+            month = checkMonth(monthValue + month)
+
+        } else {
+            self.year = 2023;
+            self.month = 1;
+            self.day = 1;
+        }
+    }
+    this.setYear = function (arg) {
+        self.year = arg;
+    }
+    this.checkMonth = function (arg) {
+        while (arg >= 11) {
+            if (month >= 11) {
+                self.month = month % 11;
+                self.year++;
+            }
+            arg -= 11;
+            month += 11;
+            self.year++;
+        }
+        month = arg;
+    }
+    this.checkDays = function (arg) {
+        while (arg > daysInMonth(month)) {
+            if (self.month == 11) {
+                self.month = 0;
+                self.year++;
+            }
+            arg -= daysInMonth(month);
+            self.month++;
+        }
+        self.days = arg;
+        self.month = daysInMonth(month);
+
+    }
+    this.checkHour = function (arg) {
+        if (arg < 24) {
+            self.year = 1970;
+            self.hour = arg;
+        } else {
+            days = parseInt(arg / 24);
+            if (days > daysInMonth(month)) {
+                checkDays(days);
+            } else {
+                self.days = days;
+            }
+            self.hour = arg % 24;
+        }
+    }
+    this.checkMinutes = function (arg) {
+        if (arg < 60) {
+            self.year = 1970;
+            self.minutes = minutes;
+        } else {
+            hour = parseInt(arg / 60);
+
+            if (hour > 24) {
+                checkHour(hour)
+                minutes = arg % 60;
+            } else {
+                self.hour = hour
+            }
+
+        }
+    }
+    this.second = function (arg = 0) {
+        if (arg < 60 || !arg) {
+            self.year = 1970;
+            self.minutes = arg;
+            self.second = arg;
+
+        } else {
+            minutes = parseInt(arg / 60);
+
+            if (minutes > 60) {
+                checkMinutes(minutes)
+            } else {
+                self.minutes = minutes
+            }
+            return arg % 60;
+        }
+    }
+    function daysInMonth(month) {
+        let daysInMonth = new Map([[0, 31], [1, 28], [2, 31],
+        [3, 30], [4, 31], [5, 30],
+        [6, 31], [7, 31], [8, 30],
+        [9, 31], [10, 30], [11, 31],])
+        return daysInMonth.get(month)
+    }
+
+
+    function checkYear() {
+        if (year === undefined) {
+            return (new Date()).getFullYear();
+        } else if (typeof year === "string") {
+            let corYearType = correctYearString(year)
+            return parseInt(corYearType.split(",")[0]);
+
+        } else {
+            return year;
+        }
+    }
     function correctYearString(stringYear) {
         let result = "";
         for (let index = 0; index < stringYear.length; index++) {
@@ -28,189 +141,34 @@ function MyDate(year, month, date, hour, minuts, second) {
         return result;
     }
 
-    function checkYear() {
-        if (year === undefined) {
-            return new Date();
-        } else if (typeof year === "string") {
-            let corYearType = correctYearString(year)
-            return parseInt(corYearType.split(",")[0]);
 
-        } else if (typeof year === "number" && typeof month === "undefined") {
-            return new Date(0);
-        } else {
-            return year;
+    this.getFullYear = function (arg) {
+        if (!arg) {
+            second(1)
+            console.log(`${strMonth[this.getMonth()]} ${self.days} ${self.year} ${self.getHour()}:${self.getMinutes()}:${this.getSeconds()}`);
         }
+        this.setYear(arg);
+        self.minutes = 0;
+        self.second = 0;
+        return self.year;
     }
-
-
-    function checkMonth() {
-        if (typeof year === "string") {
-            let corYearType = correctYearString(year)
-
-            let checkMonth = parseInt(corYearType.split(",")[1]);
-            console.log(checkMonth);
-            return correctMonth(checkMonth)
-        } else if (typeof month === "number") {
-            return correctMonth(month)
-        }
-    }
-
-    function correctMonth(month) {
-        if (month > 11) {
-            self.year += parseInt(month / 11);
-            if (month % 11 == 0) {
-                return 0;
-            }
-            return parseInt(month % 11) - 1;
-        } else if (month < 0) {
-            self.year += parseInt(month / 11);
-            return (month * -1 % 11)
-        }
-        return month;
-    }
-
-    function daysInMonth(month) {
-        let daysInMonth = new Map([[0, 31], [1, 28], [2, 31],
-        [3, 30], [4, 31], [5, 30],
-        [6, 31], [7, 31], [8, 30],
-        [9, 31], [10, 30], [11, 31],])
-        return daysInMonth.get(month)
-    }
-
-    function checkDate() {
-        if (typeof year === "string") {
-            let corYearType = correctYearString(year)
-
-            let checkDate = parseInt(corYearType.split(",")[2]);
-            return correctDate(checkDate)
-        } else if (typeof date === "number") {
-            return correctDate(date)
-        }
-    }
-
-    function correctDate(date) {
-        let dayInMonth = daysInMonth(self.month);
-        if (date > dayInMonth) {
-            self.month += parseInt(date / dayInMonth);
-            self.month = correctMonth(self.month)
-
-            if (date % dayInMonth == 0) {
-                return 1;
-            }
-            return date % dayInMonth;
-        } else if (date < 0) {
-            let modDate = date % dayInMonth;
-            let d = parseInt(date / dayInMonth);
-
-            if (self.month + d >= 0) {
-                self.month += d;
-            } else {
-                self.month = (11 + (d % 11))
-                self.year += (d % 11)
-            }
-            return modDate * -1;
-        }
-        return date;
-    }
-
-
-    this.setFullYear = function (year, month, date) {
-        if (typeof year != "number" && typeof month != "number" && typeof date != "number") {
-            this.year = NaN;
-            this.month = NaN;
-            this.date = NaN;
-
-        } else if (typeof year === "number") {
-            if (year > 1970) {
-                this.year = (year - 1970) * 24 * 3600 * 1000 * 365
-
-            } else {
-                this.year = (year + 1970) * 24 * 3600 * 1000
-
-            }
-        } else if (typeof month === "number") {
-            checkMonth(month)
-        } else if (typeof date === "number") {
-            checkDate(date)
-        }
+    this.getYear = function () {
         return this.year;
-
     }
-
-
-
-    this.getFullYear = function () {
-        if (typeof year === "undefined" || typeof year === "number" && typeof month === "undefined") {
-            return 1970;
-        }
-        return this.year;
-
-    }
-
     this.getMonth = function () {
-        if (typeof year === "undefined" || typeof year === "number" && typeof month === "undefined") {
-            return 0;
-        }
         return this.month;
     }
-
-    this.getDate = function () {
-        if (typeof year === "undefined" || typeof year === "number" && typeof month === "undefined") {
-            return 1;
-        } else if (typeof this.date === "undefined") {
-            return 1;
-        }
-        return this.date;
+    this.getDays = function () {
+        return (this.days);
     }
-
-    this.getHours = function () {
-        
-        if (typeof year === "undefined" || typeof year === "number" &&
-            typeof month === "undefined" || typeof date === "undefined") {
-            return 0;
-
-        } else if (typeof year === "number" && typeof month === "number" &&
-            typeof date === "number" && typeof hour === "undefined") {
-            return 0
-        }
-        return "23";
+    this.getHour = function () {
+        return (self.hour < 10) ? "0" + self.hour : self.hour;
     }
-
     this.getMinutes = function () {
-        if (typeof year === "undefined" || typeof year === "number" &&
-            typeof month === "undefined" || typeof this.date === "undefined") {
-            return 0;
-        } else if (typeof year === "number" && typeof month === "number" &&
-            typeof this.date === "number" && typeof hour === "undefined") {
-            return 0
-        }
-        // return this.date;
+        return (self.minutes < 10) ? "0" + self.minutes : self.minutes;
     }
-
     this.getSeconds = function () {
-        if (typeof year === "undefined" || typeof year === "number" &&
-            typeof month === "undefined" || typeof this.date === "undefined") {
-            return 0;
-        } else if (typeof year === "number" && typeof month === "number" &&
-            typeof this.date === "number" && typeof hour === "undefined") {
-            return 0
-        }
-        // return this.date;
+        return (self.second < 10) ? "0" + self.second : self.second;
     }
 
 }
-
-
-let md = new MyDate(2000);
-// console.log("md.getMyFullYear()====" + md.getFullYear());
-// console.log("md.getMyMonth() =====" + md.getMonth());
-// console.log("md.getMyDate() ======" + md.getDate());
-// console.log("md.getMyHours() ====" + md.getHours());
-// console.log("md.getMyMinute()====== " + md.getMinutes());
-// console.log("md.getMySecond()===== " + md.getSeconds());
-
-// md.setFullYear(2000)
-// console.log("md.setFullYear()===== " + md);
-
-console.log("md.setFullYear()===== " + md.setFullYear(2000));
-
