d1c4a181fe1ce5a4c4ba998b9306157b6f88d737
Manasyanrs <radikmanasyan@gmail.com>
2023-03-06 13:27:47 +0400

    fixed setters

--- class/class.js
+++ class/class.js
@@ -1,140 +1,112 @@
 "use strict"
 
 class MyDate {
-    _year=0;
-    _month=0;
-    _day=0;
-    _hour=0;
-    _minutes = 0;
-    _second = 0;
+    // _year = 0;
+    // _month = 0;
+    // _day = 0;
+    // _hour = 0;
+    // _minutes = 0;
+    // _second = 0;
     _strMonth = ["Yan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     _deysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    constructor(year, month, day, hour, minutes, second) {
-        this._setYear = year;
-        this._setMonth = month;
-        this._setDay = day;
-        this._setHour = hour;
-        this._setMinutes = minutes;
-        this._setSecond = second;
+    constructor(year=1970, month=0, day=1, hour=0, minutes=0, second=0) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minutes = minutes;
+        this.second = second;
 
     }
 
-    set _setFullYear(y) {
+    set setFullYear(y) {
         if (typeof y != "string") {
-            this._year = y;
+            this.year = y;
         } else {
             let arg = y.split(",");
+
             if (arg[0]) {
-                this._year = parseInt(arg[0]);
+                this.year = parseInt(arg[0]);
             }
             if (arg[1]) {
-                this._month = 0;
-                this._setMonth = parseInt(arg[1]);
+                this.month += parseInt(arg[1]);
+                this.setMonth = parseInt(this.month);
             } else {
-                this._month = 0;
+                this.month = 0;
             }
             if (arg[2]) {
-                this._day = 0;
-                this._setDay = parseInt(arg[2]);
+                this.day += parseInt(arg[2]);
+                this.setDay = parseInt(this.day);
             } else {
-                this._day = 1;
+                this.day = 1;
             }
         }
     }
 
-    setFullYearr(y) {
-        this._setFullYear = y;
-    }
-
-
-    set _setYear(arg) {
+    set setYear(arg) {
         if (arg) {
-            this._year = arg;
+            this.year = arg;
         }
     }
-    setYear(y) {
-        this._setYear = y;
-    }
 
-    set _setMonth(arg) {
-        this._month += arg;
-        if (this._month) {
-            while (this._month > 11) {
-                this._year++;
-                if (this._month == 11) {
-                    this._month = 0;
+    set setMonth(arg) {
+        this.month += arg;
+        if (this.month) {
+            while (this.month > 11) {
+                this.year++;
+                if (this.month == 11) {
+                    this.month = 0;
                 }
-                this._month -= 11;
+                this.month -= 11;
             }
         }
     }
-    setMonth(v) {
-        this._setMonth = v;
-    }
 
-    set _setDay(arg) {
-        this._day += arg;
+    set setDay(arg) {
+        this.day += arg;
 
-        if (this._day) {
-            while (this._day > this._deysInMonth[this._month]) {
-                if (this._month == 11) {
-                    this._month = 0;
-                    this._year++;
+        if (this.day) {
+            while (this.day > this._deysInMonth[this.month]) {
+                if (this.month == 11) {
+                    this.month = 0;
+                    this.year++;
                 }
-                this._day -= this._deysInMonth[this._month];
-                this._month++;
+                this.day -= this._deysInMonth[this.month];
+                this.month++;
             }
         }
     }
-    setDay(d) {
-        this._setDay = d;
-    }
 
-    set _setHour(arg) {
-        this._hour += arg;        
-        if (this._hour > 24) {
-            this._hour = arg % 24;
-            this._setDay = parseInt(arg / 24);
+    set setHour(arg) {
+        this.hour += arg;
+        if (this.hour > 24) {
+            this.hour = arg % 24;
+            this.setDay = parseInt(arg / 24);
         }
     }
-    setHour(h){
-        this._setHour = h;
-    }
 
-    set _setMinutes(arg) {
-        this._minutes += arg;
-        if (this._minutes > 60) {
-            this._minutes = arg % 60;
-            this._setHour = parseInt(arg / 60);
+    set setMinutes(arg) {
+        this.minutes += arg;
+        if (this.minutes > 60) {
+            this.minutes = arg % 60;
+            this.setHour = parseInt(arg / 60);
         }
 
     }
 
-    setMinutes(minut){
-        this._setMinutes = minut;
-    }
-
-    set _setSecond(arg) {
+    set setSecond(arg) {
         if (arg < 60 || !arg) {
-            this._second = arg;
+            this.second = arg;
 
         } else {
-            this._setMinutes = (parseInt(arg / 60));
-            this._second = arg % 60;
+            this.setMinutes = (parseInt(arg / 60));
+            this.second = arg % 60;
         }
     }
-    setSecond(s){
-        this._setSecond = s;
-    }
 
-    get _getFullYear() {
-        return `${this._strMonth[this._month]} ${this._day} ${this._year} ${this._hour}:${this._minutes}:${this._second}`
-    }
-
-    getFullYear(){
-        return `${this._strMonth[this._month]} ${this._day} ${this._year} ${this._hour}:${this._minutes}:${this._second}`
-
+    get getFullYear() {
+        return `${this._strMonth[this.month]} ${this.getDay} ${this.year} ${this.hour}:${this.minutes}:${this.second}`
     }
 
     get getYear() {
@@ -163,7 +135,8 @@ class MyDate {
 
 
 let m = new MyDate(2000, 2, 30, 25, 32, 30);
-m.setFullYearr("2004, 10, 10");
-m.setMinutes(90)
-m.setDay(90)
-console.log(m.getFullYear());
+m.setFullYear = "2004, 10, 10";
+m.setMinutes = 2990;
+m.setDay = 90;
+console.log(m.getFullYear);
+console.log(m);
